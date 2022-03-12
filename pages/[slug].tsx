03ec316a10f884a2aa439b640ad6a99ea4ce9824
l3luescreen <hacky404@hotmail.com>
2022-03-13 00:34:46 +0700

    Add Post info page

---
+++ pages/[slug].tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+import type { ApiPost, Author, AxiosResult } from './Types/wordpress'
+import Head from './components/Head'
+import Navbar from './components/Navbar'
+import Content from './components/Content'
+
+const axios = require('axios')
+
+const Post: NextPage = () => {
+    const router = useRouter()
+    const slug = router.query.slug
+    const [postInfo, setPostInfo] = useState<ApiPost>()
+
+    const payload = {
+        params: {
+            slug
+        },
+        headers: {
+            Authorization: process.env.NEXT_PUBLIC_AUTHORIZATION
+        }
+    }
+
+    useEffect(() => {
+        axios
+            .get(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/posts`, payload)
+            .then((post: AxiosResult) => {
+                setPostInfo(post.data[0])
+            })
+            .catch((error: any) => console.error(error))
+    }, [])
+
+    return (
+        <>
+            <Head title="Blognil" />
+            <Navbar />
+            <div className="container mx-auto p-20">
+                <Content
+                    title={postInfo?.title.rendered}
+                    author={postInfo?.author}
+                    content={postInfo?.content.rendered}
+                    date={postInfo?.modified}
+                />
+            </div>
+        </>
+    )
+}
+
+export default Post
