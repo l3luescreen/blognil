c8e46471d38c728cfae52fee27dc368803bc7e18
l3luescreen <hacky404@hotmail.com>
2022-03-13 21:53:15 +0700

    Make post still post that match to url when reload

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-import type { ApiPost, Author, AxiosResult } from './Types/wordpress'
+import type { ApiPost, Author, AxiosResult } from '../Types/wordpress'
 import Head from './components/Head'
 import Navbar from './components/Navbar'
 import Content from './components/Content'
@@ -11,26 +11,27 @@ const axios = require('axios')
 
 const Post: NextPage = () => {
     const router = useRouter()
-    const slug = router.query.slug
     const [postInfo, setPostInfo] = useState<ApiPost>()
 
-    const payload = {
-        params: {
-            slug
-        },
-        headers: {
-            Authorization: process.env.NEXT_PUBLIC_AUTHORIZATION
-        }
-    }
-
     useEffect(() => {
-        axios
-            .get(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/posts`, payload)
-            .then((post: AxiosResult) => {
-                setPostInfo(post.data[0])
-            })
-            .catch((error: any) => console.error(error))
-    }, [])
+        const slug = router.query.slug
+        const payload = {
+            params: {
+                slug
+            },
+            headers: {
+                Authorization: process.env.NEXT_PUBLIC_AUTHORIZATION
+            }
+        }
+        if (router.query.slug) {
+            axios
+                .get(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/posts`, payload)
+                .then((post: AxiosResult) => {
+                    setPostInfo(post.data[0])
+                })
+                .catch((error: any) => console.error(error))
+        }
+    }, [router.query.slug])
 
     return (
         <>
